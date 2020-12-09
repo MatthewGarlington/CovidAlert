cfd7527fda33485f8871b0b7299673e3af7d096e
Matthew Garlington <SBMatthewG@gmail.com>
2020-12-09 10:30:07 -0500

    Initial Commit

---
+++ CovidAlert/CovidAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  CovidAlertApp.swift
+//  CovidAlert
+//
+//  Created by Matthew Garlington on 12/9/20.
+//
+
+import SwiftUI
+
+@main
+struct CovidAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
